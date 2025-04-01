7dc3cc9777f54a718668be09cbaf901ce6f3c549
Demir Cücü <demircucu35@gmail.com>
2025-04-02 02:27:26 +0300

    JavaFX and Gradle Updates -The project was changed from a normal Java module to a Gradle module. -Gradle tests were conducted. -The JSON library was added. -The basic design of the main screen was created. -The following pages were added: -- Store Page (Basic) -- Library Page (Basic) -- JSON File Page (Basic) -- Help Page (Basic) -- Account Page (Basic)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.ce216.gamecatalog {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.json;
 
 
     opens org.ce216.gamecatalog to javafx.fxml;
---
+++ src/main/java/org/ce216/gamecatalog/UIManager.java
@@ -0,0 +1,40 @@
+package org.ce216.gamecatalog;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
+
+
+public class UIManager extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("main-screen.fxml"));
+            Parent root = loader.load();
+
+            primaryStage.setTitle("Main Screen");
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
