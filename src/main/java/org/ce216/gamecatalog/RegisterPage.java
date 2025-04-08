3cc510634b4ca874afd316086087bbeb9fb1b99e
Ahmet Cenker Altın <ahmetcenkeraltin@gmail.com>
2025-04-08 19:04:06 +0300

    Add initial structure for Login and Register pages
    Implemented basic layout for login and registration pages. Username and password etc. authentication functionality will be added in a future update.

---
+++ src/main/java/org/ce216/gamecatalog/RegisterPage.java
@@ -0,0 +1,26 @@
+package org.ce216.gamecatalog;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class RegisterPage {
+    @FXML
+    private Stage stage;
+    @FXML
+    private Scene scene;
+
+    public void switchScenetoMainMenu(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main-screen.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.setTitle("Main Screen");
+        stage.show();
+    }
+}
